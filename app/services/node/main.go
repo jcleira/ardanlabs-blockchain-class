593234f7617bdb16593e51859ffc389c157a6cc2
jcleira <jmc.leira@gmail.com>
2022-05-23 20:07:39 +0200

    Add genesis configuration

--- app/services/node/main.go
+++ app/services/node/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/ardanlabs/blockchain/app/services/node/handlers"
+	"github.com/ardanlabs/blockchain/foundation/blockchain/genesis"
 	"github.com/ardanlabs/blockchain/foundation/logger"
 	"github.com/ardanlabs/conf/v3"
 	"go.uber.org/zap"
@@ -94,6 +95,16 @@ func run(log *zap.SugaredLogger) error {
 		return fmt.Errorf("generating config for output: %w", err)
 	}
 	log.Infow("startup", "config", out)
+
+	// =========================================================================
+	// Blockchain Support
+
+	genesis, err := genesis.Load()
+	if err != nil {
+		return fmt.Errorf("genesis load: %w", err)
+	}
+
+	log.Infow("startup", "gen", genesis)
 
 	// =========================================================================
 	// Service Start/Stop Support
